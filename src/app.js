53089561ae6364d957a0fb0792d5ba60d7e3dd0c
Cohen <cohenud@merck.com>
2026-05-23 15:29:58 +0300

    Serve static web UI from public/ folder

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const personsRouter = require('./routes/persons');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/health', (_req, res) => res.json({ status: 'ok' }));
 app.use('/persons', personsRouter);
